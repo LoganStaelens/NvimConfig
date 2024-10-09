af7a1ae7a7b23c44a5ea93d637f2be0306a31972
Logan Staelens <lost@odoo.com>
2024-10-09 18:11:20 +0200

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,19 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Remove autoformat on save
+vim.g.autoformat = false
+
+-- Tab Width
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+-- Line Wrap
+vim.o.wrap = false
+
+-- Line numbers
+vim.o.number = true
+vim.o.relativenumber = false
